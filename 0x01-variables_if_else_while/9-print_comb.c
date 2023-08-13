9816a81e7ed34b0bc21373810149ce34f673cda9
Damxxiii <callmedom23@gmail.com>
2023-08-13 12:00:45 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,14 +12,9 @@ int main(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		if (i <= 9)
-			putchar(i + '0');
-		else
-		{
-			putchar(i);
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
 	}
 	return (0);
 }
