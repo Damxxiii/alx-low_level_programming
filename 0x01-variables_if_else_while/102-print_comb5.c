c583caef5d7b9bfce0122817aa9e11cf6227bbe9
Damxxiii <callmedom23@gmail.com>
2023-08-13 15:07:34 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,8 +8,7 @@
 
 int main(void)
 {
-	int num1;
-	int num2;
+	int num1, num2;
 
 	for (num1 = 0 ; num1 <= 98 ; num1++)
 	{
@@ -18,13 +17,12 @@ int main(void)
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
 			putchar(' ');
-
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
 
 			if (num1 == 98 && num2 == 99)
-
-				putchar(',');
+				continue;
+			putchar(',');
 			putchar(' ');
 		}
 	}
