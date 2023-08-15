36a202295481fa913e692e7a357a6acef6ba669a
Damxxiii <callmedom23@gmail.com>
2023-08-15 15:23:32 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - computes the absolute value of an integer.
+ * @n: the int to be checked
+ * Return: the absolue value of int
+ */
+
+int main(void)
+{
+	int n;
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
