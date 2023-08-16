49a64436ebf2afe2d93d4424b962c679b301e45b
Damxxiii <callmedom23@gmail.com>
2023-08-15 20:10:52 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,11 +10,12 @@
 
 int main(void)
 {
-	int i, sum = 0;
+	int i;
+	int sum = 0;
 
 	for (i = 0 ; i < 1024; i++)
 	{
-		if ((i % 3) == 0 || (i % 5) == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 			sum = sum + 1;
 	}
 	printf("%d\n", sum);
