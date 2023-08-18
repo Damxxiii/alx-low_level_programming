693fca453f54dfc7d74c466d474ef3114426a64a
Damxxiii <callmedom23@gmail.com>
2023-08-18 11:28:25 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line.
+ * @size: size of squares
+ * Return: void
+ */
+
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (i = 0 ; i < size ; i++)
+		{
+			for (j = size - i ; j > 1 ; j--)
+			{
+				putchar(32);
+			}
+
+			for (k = 0 ; k <= i ; k++)
+			{
+				putchar(35);
+			}
+
+			putchar('\n');
+		}
+	}
+
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ * followed by a new line.
+ * Return: void
+ */
+
+#include <unistd.h>
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c >= 0 ; c <= 9 ;, c++)
+	{
+		if (c != 2 && c != 4)
+			_putchar(c + '0');
+	}
+	_putchar('\n');
+}
