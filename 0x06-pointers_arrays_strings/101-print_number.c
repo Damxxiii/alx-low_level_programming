3857da5c892e1295e351f769810ec388a7f51b53
Damxxiii <callmedom23@gmail.com>
2023-08-24 18:16:11 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_number - prints an integer.
- * @n: integer
+ * print_number - print numbers chars
+ * @n: integer params
  * Return: 0
  */
 
@@ -18,9 +18,11 @@ void print_number(int n)
 		putchar('-');
 		n1 = -n;
 	}
+
 	if (n1 / 10 != 0)
 	{
 		print_number(n1 / 10);
 	}
 	putchar((n1 % 10) + '0');
 }
+
