9e014e4e825535d6aa41bdb755a8322aa73bfb9a
Damxxiii <callmedom23@gmail.com>
2023-08-28 17:06:02 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,4 +2,26 @@
 
 /**
  * _strstr -  a function that locates a substring.
- *
+ * @needle: input
+ * @haystack: input
+ * Return: 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0' ; haystack++)
+	{
+		char *a = haystack;
+		char *b = needle;
+
+		while (*a == *b && *b != '\0')
+		{
+			a++;
+			b++;
+		}
+		if (*b == '\0')
+			return (haystack);
+	}
+	return (0);
+}
+
