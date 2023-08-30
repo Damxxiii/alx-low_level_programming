6d4274d86c25ca57698940d073062337aae46c73
Damxxiii <callmedom23@gmail.com>
2023-08-30 11:48:07 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number, otherwise return 0.
+ * @n: integer
+ * Return: 1 on success, otherwise 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n / n == 1)
+	{
+		return (1);
+	}
+	else if
+}
