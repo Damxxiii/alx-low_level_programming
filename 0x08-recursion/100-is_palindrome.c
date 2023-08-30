3e394ad3050278df6b2a152eb769b7bfaf9270cf
Damxxiii <callmedom23@gmail.com>
2023-08-30 14:10:41 -0700

    101-wildcmp.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,37 +1,32 @@
 #include "main.h"
-#include <string.h>
 /**
- * _strlen - string length
+ * _strlen_recursion - string length
  * @s: string
  * Return: length of string
  */
 
-int _strlen(char *s)
+int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-	{
 		return (0);
-	}
 	else
-	{
-		return (1 + _strlen(s + 1));
-	}
+		return (1 + _strlen_recursion(s + 1));
 }
 /**
  * compare_string - compare each string character
  * @s: string
- * @l: smallest iterator
- * @r: largest iterator
+ * @left: smallest iterator
+ * @right: largest iterator
  * Return: int
  */
 
-int compare_string(char *s, int l, int r)
+int compare_string(char *s, int left, int right)
 {
-	if (*s + l == (*s + r))
+	if (*s + left == (*s + right))
 	{
-		if (l == r || l == r + 1)
+		if (left == right || left == right + 1)
 			return (1);
-		return (0 + compare_string(s, l + 1, r - 1));
+		return (0 + compare_string(s, left + 1, right - 1));
 	}
 	return (0);
 }
@@ -45,5 +40,5 @@ int is_palindrome(char *s)
 {
 	if (*s == '\0')
 		return (1);
-	return (compare_string(s, 0, strlen(s) - 1));
+	return (compare_string(s, 0, _strlen_recursion(s) - 1));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,6 +5,8 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
-int compare_string(char *s, int l, int r);
-int is_palindrome(char *s);
+int compare_string(char *s, int left, int right);
+int _strlen_recursion(char *s);
+int wildcmp(char *s1, char *s2);
+
 #endif
