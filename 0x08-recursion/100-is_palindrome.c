f97820b795dec249d93e66649a709c59923d7f10
Damxxiii <callmedom23@gmail.com>
2023-08-30 16:51:04 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,7 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strlen_recursion - to get the string length
+ * is_palindrome - checks if string is a palindrome
+ * @s: string to reverse
+ * Return: 1 on success, otherwise 0
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - returns length of string
  * @s: string
  * Return: length of string
  */
@@ -10,36 +24,22 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
+	return (1 + _strlen_recursion(s + 1));
 }
+
 /**
- * compare_string - compare each string character
- * @s: string to compare
- * @left: smallest iterator
- * @right: largest iterator
- * Return: int
+ * check_pal - to get the string length
+ * @s: string
+ * @i: iterator
+ * @len: string length
+ * Return: 1 if palindrome, otherwise 0
  */
 
-int compare_string(char *s, int left, int right)
+int check_pal(char *s, int i, int len)
 {
-	if (*s + left == *(s + right))
-	{
-		if (left == right || left == right + 1)
-			return (1);
-		return (0 + compare_string(s, left + 1, right - 1));
-	}
-	return (0);
-}
-/**
- * is_palindrome - check if a string is a palindrome
- * @s: string to test
- * Return: 1 if it is a palindrome, otherwise 0
- */
-
-int is_palindrome(char *s)
-{
-	if (*s == '\0')
+	if (*(s + 1) != *(s + len - 1))
+		return (0);
+	if (i >= len)
 		return (1);
-	return (compare_string(s, 0, _strlen_recursion(s) - 1));
+	return (check_pal(s, i + 1, len - 1));
 }
