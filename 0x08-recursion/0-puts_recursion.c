6cd658fea8097cb2e99b440b0f2cf971097228af
Damxxiii <callmedom23@gmail.com>
2023-08-30 14:55:34 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+#include <unistd.h>
 /**
  * _puts_recursion -  prints a string, followed by a new line.
  * @s: pointer to the string
- * REturn: void
+ * Return: void
  */
 
 void _puts_recursion(char *s)
