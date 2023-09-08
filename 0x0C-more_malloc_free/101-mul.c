fac8240b6d7fda0a7e88b2954c7346f9d58221f7
Damxxiii <callmedom23@gmail.com>
2023-09-07 17:38:35 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -57,14 +57,14 @@ void error(void)
  */
 int main(int argc, char *argv[])
 {
-	char *s1, *s2;
+	char *num1, *num2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+	num1 = argv[1], num2 = argv[2];
+	if (argc != 3 || !is_digit(num1) || !is_digit(num2))
 		error();
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
+	len1 = _strlen(num1);
+	len2 = _strlen(num2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
 	if (!result)
@@ -73,11 +73,11 @@ int main(int argc, char *argv[])
 		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len--)
 	{
-		digit1 = s1[len] - '0';
+		digit1 = num1[len] - '0';
 		carry = 0;
 		for (len2 = len2 - 1; len2 >= 0; len2--)
 		{
-			digit2 = s2[len2] - '0';
+			digit2 = num2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
