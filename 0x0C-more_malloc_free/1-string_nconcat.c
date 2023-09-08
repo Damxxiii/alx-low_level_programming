a10dfe1f986936f30d6170a33c2afa3ad437b5c5
Damxxiii <callmedom23@gmail.com>
2023-09-07 17:19:36 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		j = 0;
 	else
 	{
-		for (j = 0; s2[j] != '\0'; j++)		;
+		for (j = 0; s2[j] != '\0'; j++)
+			;
 	}
 	if (j > n)
 		j = n;
