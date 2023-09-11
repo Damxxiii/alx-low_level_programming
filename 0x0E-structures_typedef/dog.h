c52f847d0a6c1be42a2cb8060fa7766bcf761829
Damxxiii <callmedom23@gmail.com>
2023-09-11 14:07:51 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - a struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: struct ALX
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
