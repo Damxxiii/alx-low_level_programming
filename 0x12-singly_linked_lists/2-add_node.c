7d9ddd29e8ab496ff7794e189dd28fd2b9780c33
Damxiii <callmedom23@gmail.com>
2023-09-22 09:17:12 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,28 +8,28 @@ int len(const char *str);
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *node1;
+	list_t *new_node;
 
 	if (head == NULL)
 		return (NULL);
-	node1 = malloc(sizeof(list_t));
-	return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
 	if (*head == NULL)
-		node1->next = NULL;
+
+		new_node->next = NULL;
 	else
-	{
-		node1->next = *head;
-		node1->str = strdup(str);
-		node1->len = len(str);
-		*head = node1;
-	}
+			new_node->next = *head;
+		new_node->str = strdup(str);
+		new_node->len = len(str);
+		*head = new_node;
 	return (*head);
 }
 /**
  * len - get str length
  * @str: string
- * Return: lenfth of string
+ * Return: length of string
  */
 int len(const char *str)
 {
