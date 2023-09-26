af133506da12b54686c96d302e7d634a442a4b79
Damxiii <callmedom23@gmail.com>
2023-09-26 16:21:02 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 /**
- * get_nodeint_at_index - function that returns the nth node of a listint_t linked list.
+ * get_nodeint_at_index - function that returns the
+ * nth node of a listint_t linked list.
  * @head: ptr to first element of list
  * @index: node position
  * Return: ptr to node
