fa64f48ea71cdb7a33c01ddede9a17de91c86f23
Damxiii <callmedom23@gmail.com>
2023-09-26 14:14:26 -0700

    prints all the elements of a listint_t list.

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,19 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct list_s - struct for creating nodes
+ * @n: integer
+ * @next: pointer to the next node
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+#endif
