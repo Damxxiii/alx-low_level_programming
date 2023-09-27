cbe1dda5f9d01acff31969a16cb0664caeab4078
Damxiii <callmedom23@gmail.com>
2023-09-26 18:45:04 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,14 +7,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	unsigned int count;
+	unsigned int i;
 
 	if (h == NULL)
 		return (0);
-	for (count = 0; h != NULL; count++)
+	for (i = 0; h != NULL; i++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	return (count);
+	return (i);
 }
