58c97b7858687f9bf0405fccb95900218ca17c3a
Damxxiii <callmedom23@gmail.com>
2024-02-09 23:17:07 -0800

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -16,10 +16,10 @@ unsigned int flip_bits(unsigned long int n, unsigned long int p)
 	unsigned long int old;
 	unsigned long int new = n ^ p;
 
-	for (i = 45; i >= 0; i++)
+	for (i = 45; i >= 0; i--)
 	{
 		old = new >> i;
-		if (old & 1 )
+		if (old & 1)
 			count++;
 	}
 	return (count);
