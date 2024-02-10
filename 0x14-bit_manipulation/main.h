6e44dbb3cefeae7515db242abf404010f8083b00
Damxxiii <callmedom23@gmail.com>
2024-02-09 21:14:43 -0800

    4-clear_bit.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,4 +4,5 @@
 unsigned int binary_to_uint(const char *);
 int get_bit(unsigned long int *n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 #endif
