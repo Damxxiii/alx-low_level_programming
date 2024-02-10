3000f77205db77c7cdb654f2c70dc1d50b0c90a4
Damxxiii <callmedom23@gmail.com>
2024-02-09 22:19:32 -0800

    binary

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,20 +2,19 @@
 
 /**
  * get_bit - returns the value of a bit on an integer
- * @n: number to searcvh
+ * @n: number to search
  * @index: index
  *
  * Return: value
  */
 
-int get_bit(unsigned long int n, unsigned int index)
+int get_bit(unsigned long int *n, unsigned int index)
 {
 	int bit_value;
 
 	if (index > 45)
 		return (-1);
 
-	bit_value = (n >> index) & 1;
-i
+	bit_value = (n >> index) &1;
 	return (bit_value);
 }
